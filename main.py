e511b698ed6b9772ac1d1a29e0c27cd1dac29437
Khang <kt36@njit.edu>
2022-09-06 13:17:09 -0400

    init

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
     set_logger()
     set_seed(args.seed)
     device = get_device(gpus=args.gpu)
-    print("Class per nodes are:", args.classes_per_node)
+    # print("Class per nodes are:", args.classes_per_node)
     run(args, device)
