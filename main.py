4a5256ab6b70c8682744c8f20d9541badce0c44d
Khang <kt36@njit.edu>
2022-09-06 14:58:13 -0400

    init

--- main.py
+++ main.py
@@ -26,22 +26,14 @@ def run(args, device):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-
+    train_loader = torch.utils.data.DataLoader(
+        AMIADatasetCelebA(target, transform, args.data_path, True, imgroot=None, multiplier=2000), shuffle=False,
+        num_workers=0, batch_size=200000)
+    test_loader = torch.utils.data.DataLoader(
+        AMIADatasetCelebA(target, transform, args.data_path, False, imgroot=None, multiplier=100), shuffle=False,
+        num_workers=0, batch_size=200000)
     if args.mode == 'train':
-        dataset_train = AMIADatasetCelebA(args=args, target=target, transform=transform, dataroot=args.data_path, mode='train',
-                                    imgroot=None, multiplier=2000)
-        dataset_valid = AMIADatasetCelebA(args=args, target=target, transform=transform, dataroot=args.data_path, mode='valid',
-                                          imgroot=None, multiplier=100)
-    else:
-        dataset = AMIADatasetCelebA(args=args, target=target, transform=transform, dataroot=args.data_path, mode='test',
-                                    imgroot=None, multiplier=args.num_multiplier)
-
-    train_data_loader = torch.utils.data.DataLoader(dataset_train, shuffle=False,
-                                              num_workers=0, batch_size=args.batch_size)
-    valid_data_loader = torch.utils.data.DataLoader(dataset_valid, shuffle=False,
-                                                    num_workers=0, batch_size=args.batch_size)
-    if args.mode == 'train':
-        train(args=args, device=device, data=(train_data_loader, valid_data_loader), model=model)
+        train(args=args, device=device, data=(train_loader, test_loader), model=model)
         # if args.train_mode == 'clean':
         #     train_clean(args=args, device=device, nodes=nodes, hnet=hnet, net=net)
         # elif args.train_mode == 'userdp':
