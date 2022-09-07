f3a9ed99d28b03f717548316bf72bbf763cd9701
Khang <kt36@njit.edu>
2022-09-07 15:28:17 -0400

    init

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def run(args, device):
     del(train_loader)
     del(valid_loader)
     gc.collect()
-    result = evaluate_robust(args=args, data = test_loader, model=model, device=device)
+    result = evaluate_robust(args=args, data = test_loader, model=model)
     print(result)
     json_object = json.dumps(result, indent=4)
     SAVE_NAME = 'CELEBA_embed_Lap_single_{}_{}_lr_{}.json'.format(args.num_target, args.epsilon, args.lr)
