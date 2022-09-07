027aa7abeaa33670e6d2b61efbbf99b6d7dbef9d
Khang <kt36@njit.edu>
2022-09-06 21:01:36 -0400

    init

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def run(args, device):
     gc.collect()
     result = evaluate_robust(args=args, data = test_loader, model=model, device=device)
     json_object = json.dumps(result, indent=4)
-    SAVE_NAME = 'CELEBA_embed_Lap_single_{}_{}.json'.format(args.num_target, args.epsilon)
+    SAVE_NAME = 'CELEBA_embed_Lap_single_{}_{}_lr_{}.json'.format(args.num_target, args.epsilon, args.lr)
     # Writing to sample.json
     with open(args.save_path + SAVE_NAME, "w") as outfile:
         outfile.write(json_object)
