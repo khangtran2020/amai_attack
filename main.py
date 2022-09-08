4c08be77649dc0177bc2bb9cb0c5d93be541e140
Khang <kt36@njit.edu>
2022-09-08 15:27:29 -0400

    init

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def run(args, device):
 
         print(results)
         json_object = json.dumps(results, indent=4)
-        SAVE_NAME = 'CELEBA_train_eps_{}_sample_rate_{}_num_step.json'.format(args.epsilon,
+        SAVE_NAME = 'CELEBA_train_eps_{}_sample_rate_{}_num_step.json'.format(args.epsilon ,
                                                                                        args.sample_target_rate,
                                                                                        args.num_steps)
         # SAVE_NAME = 'test.json'
