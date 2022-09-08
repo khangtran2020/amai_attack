bc976dd413c99a33c782a1f00fa957595f5a823e
Khang <kt36@njit.edu>
2022-09-08 17:11:53 -0400

    init

--- Model/model.py
+++ Model/model.py
@@ -73,6 +73,7 @@ def train(args, device, data, model):
     x_train, y_train, imgs_train = next(iter(train_dataloader))
     x_valid, y_valid, imgs_valid = next(iter(valid_dataloader))
     print(torch.bincount(y_train))
+    print(torch.bincount(y_valid))
     if (args.train_mode == 'dp'):
         print("Train with Laplace mechanism with epsilon = {}".format(args.epsilon))
         temp_x = x_train.numpy()
