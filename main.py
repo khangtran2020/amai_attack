a133f0309861a46799fc85713062008f760bf234
Khang <kt36@njit.edu>
2022-09-08 14:04:47 -0400

    init

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def run(args, device):
             temp_x = x_test.numpy()
             generated_target = np.tile(temp_x[0, :], (args.num_draws + 1, 1))
             generated_target[1:, :] = generated_target[1:, :] + np.random.laplace(0,
-                                                                                  args.sens * args.num_feature / eps,
+                                                                                  args.sens * args.num_feature / args.epsilon,
                                                                                   generated_target[1:, :].shape)
             temp_x = torch.from_numpy(generated_target.astype(np.float32)).to(device)
             out, probs, fc2 = model(temp_x)
