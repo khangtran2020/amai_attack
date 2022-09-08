68d9a82471e27e26f2a3d8ad0c0fd40a379abcd5
Khang <kt36@njit.edu>
2022-09-08 13:27:28 -0400

    init

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def run(args, device):
                 'has_target': int(sample[0]),
                 'predicted': int(bool(min(1, sum(pred.cpu().numpy()))))
             }
-        tpr, tnr, acc, = tpr_tnr_true(prediction=torch.from_numpy(np.array(predicted).astype(int)),
+        tpr, tnr, acc, = tpr_tnr(prediction=torch.from_numpy(np.array(predicted).astype(int)),
                                       truth=torch.from_numpy(np.array(true_label).astype(int)))
         results['test_result'] = {
             'acc': acc,
