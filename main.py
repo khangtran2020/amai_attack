57b58111a7620664305119eac738cdf4cc8b719d
Khang <kt36@njit.edu>
2022-09-14 11:50:22 -0400

    init

--- main.py
+++ main.py
@@ -35,8 +35,10 @@ def run(args, target, device):
         AMIADatasetCelebA(args=args, target=target, transform=transform, dataroot=args.data_path, mode='valid', imgroot=None, multiplier=args.valid_multiplier),
         shuffle=False,
         num_workers=0, batch_size=200000)
+    print("Sensitivity: {}, Number of features: {}, epsilon used in training: {}".format(args.sens, args.num_feature,
+                                                                                         args.epsilon))
     model = train(args=args, target=target, device=device, data=(train_loader, valid_loader), model=model)
-    print("Sensitivity: {}, Number of features: {}, epsilon used in training: {}".format(args.sens, args.num_feature, args.epsilon))
+
     exit()
     if args.train_mode == 'target':
         results = {}
