e034af949a3f8d5d4a8f3f729595ac11632322f2
Khang <kt36@njit.edu>
2022-09-14 22:17:03 -0400

    init

--- Model/model.py
+++ Model/model.py
@@ -175,7 +175,7 @@ def train_triplet(args, target, device, data, model):
         del (pre3)
         del (probs2)
         del (probs3)
-        loss = criteria(probs1, y_train) + args.reg*triplet_loss(f1, f2, f3)
+        loss = 1/args.reg*criteria(probs1, y_train) + args.reg*triplet_loss(f1, f2, f3)
         loss_value += loss
         predictions = pre1[:, 0] < 0
         tpr_train, tnr_train, _ = tpr_tnr(predictions, y_train)
