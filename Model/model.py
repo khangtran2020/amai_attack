421d2487b8bc37de22722d71a813238305327e2c
Khang <kt36@njit.edu>
2022-09-14 20:17:39 -0400

    init

--- Model/model.py
+++ Model/model.py
@@ -177,7 +177,7 @@ def train_triplet(args, target, device, data, model):
         del (probs3)
         loss = criteria(probs1, y_train) + triplet_loss(f1, f2, f3)
         loss_value += loss
-        predictions = fc2[:, 0] < 0
+        predictions = pre1[:, 0] < 0
         tpr_train, tnr_train, _ = tpr_tnr(predictions, y_train)
 
         loss.backward()
